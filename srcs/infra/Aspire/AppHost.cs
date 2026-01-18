b14426f52bdb1c55f9e0f51f668fef2336cf4d0f
Dat Nguyen <datntdev@outlook.com>
2026-01-18 22:04:32 +0700

    build: scaffolding microservice web api projects

--- srcs/infra/Aspire/AppHost.cs
+++ srcs/infra/Aspire/AppHost.cs
@@ -2,4 +2,12 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.datntdev_Microservice_App_Identity>("datntdev-microservice-app-identity");
 
+builder.AddProject<Projects.datntdev_Microservice_Srv_Identity_Web_Host>("datntdev-microservice-srv-identity-web-host");
+
+builder.AddProject<Projects.datntdev_Microservice_Srv_Admin_Web_Host>("datntdev-microservice-srv-admin-web-host");
+
+builder.AddProject<Projects.datntdev_Microservice_Srv_Notify_Web_Host>("datntdev-microservice-srv-notify-web-host");
+
+builder.AddProject<Projects.datntdev_Microservice_Srv_Payment_Web_Host>("datntdev-microservice-srv-payment-web-host");
+
 builder.Build().Run();
